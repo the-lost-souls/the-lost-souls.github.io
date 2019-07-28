b9d2c04fd756613761dc28508fdfac48caf5c9cd
CircleCI <torbjorv@gmail.com>
2019-07-28 00:03:27 -0700

    getting closer

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit, AfterViewInit, ViewChildren, ViewChild, ElementRef, QueryList, HostListener, ChangeDetectorRef } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { ParallaxComponent } from '../parallax/parallax.component';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import * as IsMobile from 'is-mobile';
-import { CarouselChild } from '../carousechild';
-import { DomSanitizer } from '@angular/platform-browser';
-import { Minimatch } from 'minimatch';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import * as StackBlur from 'stackblur-canvas';
 
 @Component({
   selector: 'app-main',
@@ -14,126 +11,150 @@ import { Minimatch } from 'minimatch';
 export class MainComponent implements OnInit, AfterViewInit {
 
   images = [
-    '../../assets/car.jpg',
-    '../../assets/clown_fish.jpg',
-    '../../assets/parrot.jpg',
+    '../../assets/iv.jpg',
     '../../assets/anytime.jpg',
+    '../../assets/III.jpg',
+    '../../assets/cucumber01.png',
+    '../../assets/FYH.jpg',
     '../../assets/II.jpg',
-    '../../assets/synthetic.png'
+    '../../assets/pgp.gif',
+    '../../assets/mom02.png',
+    '../../assets/pese01.png',
+    '../../assets/time01.png',
+    '../../assets/synthetic.png',
+    '../../assets/sorcerer.png',
+    '../../assets/tib.png',
   ];
 
-  @ViewChildren('child')
-  private _kids: QueryList<CarouselChild>;
-
   @ViewChild('carousel', {static: false})
-  private _carousel: ElementRef<HTMLElement>;
-
-  @ViewChild('container', {static: false})
   private _container: ElementRef<HTMLElement>;
 
-  public size: [number, number];
-  // public zoomArea: number;
+  public selectedItem = 0;
+  public itemSize: number;
+  public itemSizeStyle: string;
 
   public tops: number[] = [];
-  public centers: number[] = [];
+  public translate: number[] = [];
   public scales: number[] = [];
   public center: number;
   public carouselHeight: number;
-  public grow = 2;
+  public grow = 3;
+  public shadowCenters: number[] = [];
+  public transforms: SafeStyle[] = [];
 
-  public top = 0;
+  public scrollPaddingTop: string;
+  public scrollPaddingBottom: string;
+
+  public margins: string[] = [];
 
   constructor(
-    private _host: ElementRef<HTMLElement>,
     private _changeDetector: ChangeDetectorRef,
     private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    const min = IsMobile.isMobile(navigator.userAgent) ? 75 : 50;
-    const max = min * 2;
-
-    this.size = [min, max];
+    this.itemSize = IsMobile.isMobile(navigator.userAgent) ? 75 : 100;
+    this.itemSizeStyle = `${this.itemSize}px`;
     this.tops = new Array(this.images.length);
+    this.transforms = new Array(this.images.length);
   }
 
   ngAfterViewInit() {
     this.center = 250;
-    this.top = this.center - this.size[1] / 2;
 
-    const topFill = this.top;
+    this.margins = new Array(this.images.length);
+    this.margins[0] = `${this.center - this.itemSize / 2}px 0 0 0`;
+    this.margins[this.images.length - 1] = `0 0 ${this._container.nativeElement.clientHeight - this.center - this.itemSize / 2}px 0`;
 
-    console.log(`clientHeight: ${this._container.nativeElement.clientHeight} center: ${this.center} min: ${this.size[0]}`);
-    const bottomFill = (this._container.nativeElement.clientHeight - this.center);
+    this.scrollPaddingTop = `${this.center - this.itemSize / 2}px`;
+    this.scrollPaddingBottom = `${this._container.nativeElement.clientHeight - this.center - this.itemSize / 2}px`;
 
-    this.carouselHeight = topFill + this.images.length * this.size[0] + bottomFill;
     this.updateLayout();
-    this._changeDetector.detectChanges();
 
     this.helper();
+    this._changeDetector.detectChanges();
   }
 
   private updateLayout() {
     for (let i = 0; i < this.images.length; i++) {
-      this.centers[i] = this.center + i * this.size[0];
-      this.tops[i] = this.centers[i] - i * this.size[0];
+      this.translate[i] = this.center + i * this.itemSize;
+      this.tops[i] = this.translate[i] - i * this.itemSize;
     }
   }
 
   setSelected(i: number) {
-
-
-
-    this._container.nativeElement.scrollTo(0, i * this.size[0]);
+    this._container.nativeElement.scrollTo(0, i * this.itemSize);
   }
 
-  getTransform(top: number, scale: number) {
+  getTransform(top: number, scale: number): SafeStyle {
     const transform = `translateY(${top}px) scale(${scale})`;
     return this.sanitizer.bypassSecurityTrustStyle(transform);
   }
 
+
   helper() {
+
+    this.shadowCenters = new Array(this.images.length);
+    for (let i = 0; i < this.images.length; i++) {
+      this.shadowCenters[i] = this.center + i * this.itemSize - this._container.nativeElement.scrollTop;
+    }
 
     const distances: number[] = new Array(this.images.length);
     const centerTransformed = this._container.nativeElement.scrollTop + this.center;
     for (let i = 0; i < this.images.length; i++) {
-      distances[i] = Math.abs(centerTransformed - (this.center + i * this.size[0]));
+      distances[i] = Math.abs(centerTransformed - (this.center + i * this.itemSize));
     }
 
-    const p = (this._container.nativeElement.scrollTop % this.size[0]) / this.size[0];
+    const p = (this._container.nativeElement.scrollTop % this.itemSize) / this.itemSize;
 
     const heights: number[] = new Array(this.images.length);
-    heights.fill(this.size[0]);
+    heights.fill(this.itemSize);
 
     for (let i = 0; i < this.images.length; i++) {
-      const k = Math.min(1, distances[i] / (this.size[0] * 3));
+      const k = Math.min(1, distances[i] / (this.itemSize * 2));
 
-      const scale = 1 + this.grow * (1 + Math.cos(k * Math.PI)) / 2;
+      const scale = 1 + (this.grow - 1) * (1 + Math.cos(k * Math.PI)) / 2;
       this.scales[i] = scale;
-      heights[i] = this.size[0] * scale;
+      heights[i] = this.itemSize * scale;
     }
 
-    const a = Math.floor(this._container.nativeElement.scrollTop / this.size[0]);
+    const a = Math.floor(this._container.nativeElement.scrollTop / this.itemSize);
     const b = a + 1;
 
-    const dist = (heights[a] + heights[b]) / 2;
-    this.centers[a] = centerTransformed - p * dist;
-    this.centers[b] = centerTransformed + (1 - p) * dist;
+    // this.selectedItem = p < 0.5 ? a : b;
+    // this.selectedItem = Math.min(this.selectedItem, this.images.length - 1);
 
+    const dist = (heights[a] + heights[b]) / 2 - this.itemSize;
+    this.translate[a] = - p * dist;
+    this.translate[b] = (1 - p) * dist;
 
-    let current = this.centers[a] - heights[a] / 2;
+    let current = this.translate[a] - (heights[a] - this.itemSize) / 2;
     for (let i = a - 1; i >= 0; i--) {
-      this.centers[i] = current - heights[i] / 2;
-      current -= heights[i];
+      current -= (heights[i] - this.itemSize) / 2;
+      this.translate[i] = current;
+      current -= (heights[i] - this.itemSize) / 2;
     }
 
-    current = this.centers[b] + heights[b] / 2;
+    current = this.translate[b] + (heights[b] - this.itemSize) / 2;
     for (let i = b + 1; i < this.images.length; i++) {
-      this.centers[i] = current + heights[i] / 2;
-      current += heights[i];
+      current += (heights[i] - this.itemSize) / 2;
+      this.translate[i] = current;
+      current += (heights[i] - this.itemSize) / 2;
     }
 
     for (let i = 0; i < this.images.length; i++) {
-      this.tops[i] = this.centers[i] - this.size[0] / 2;
+      // this.tops[i] = this.translate[i] - this.itemSize / 2;
+      this.transforms[i] = this.getTransform(this.translate[i], this.scales[i]);
     }
+
+    // window.clearTimeout(this.isScrollingTimeout);
+    // this.isScrollingTimeout = window.setTimeout(
+    //   () => this._container.nativeElement.scrollTo({top: this.selectedItem * this.itemSize, behavior: 'auto'}), 500);
+
   }
+
+  hello(e) {
+    console.log(e)
+  }
+
+
 }
--- src/app/parallax/parallax.component.ts
+++ src/app/parallax/parallax.component.ts
@@ -10,11 +10,11 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./parallax.component.sass'],
   animations: [
     trigger('spin', [
-      state('true', style({
-        transform: 'translate(-50%, -50%) scale(3.5) rotate({{ begin }}) '
+      state('true, void', style({
+        transform: 'translate(-50%, -50%) rotate({{ begin }}) '
       }), { params: { begin: '213deg'}}),
-      state('false, void', style({
-        transform: 'translate(-50%, -50%) scale(3) rotate({{ end }})'
+      state('false', style({
+        transform: 'translate(-50%, -50%) rotate({{ end }})'
       }), { params: { end: '213deg' }}),
       transition('* => *', [
         animate('{{ duration }} ease-in-out')
@@ -33,11 +33,17 @@ import { DomSanitizer } from '@angular/platform-browser';
     ])
   ]
 })
-export class ParallaxComponent implements OnInit, CarouselChild {
+export class ParallaxComponent implements OnInit, CarouselChild, AfterViewInit {
 
-  public rotate1 = true;
+  public rotate1;
   public rotate2 = true;
   public zoom = true;
+
+  public angle1 = 45;
+  public angle2 = -63;
+  public scale1 = 3;
+  public scale2 = 3;
+  private _previousT: number;
 
   @Input()
   public height = 100;
@@ -66,12 +72,35 @@ export class ParallaxComponent implements OnInit, CarouselChild {
   ngOnInit() {
   }
 
-  public blurAll() {
-    this.blur(this._theimage.nativeElement, this._blurred1.nativeElement);
-    this.blur(this._theimage.nativeElement, this._blurred2.nativeElement);
+  ngAfterViewInit(): void {
+    requestAnimationFrame((frameT) => this.animate(frameT));
   }
 
-  blur(img: HTMLImageElement, canvas: HTMLCanvasElement) {
+  private animate(t: number) {
+
+    if (this._previousT) {
+      const elapsed = t - this._previousT;
+      this.angle1 += 5.5 * elapsed / 1000;
+      this.angle2 += -8.2 * elapsed / 1000;
+      this.scale1 = Math.sin(elapsed / 10000) + 2;
+      // console.log(this.scale1);
+    }
+    this._previousT = t;
+    requestAnimationFrame((frameT) => this.animate(frameT));
+  }
+
+  public blurAll() {
+    this.blur(this._theimage.nativeElement, this._blurred1.nativeElement, 7);
+    // this.blur(this._theimage.nativeElement, this._blurred2.nativeElement, 3);
+  }
+
+  getTransform(angle: number, scale: number) {
+    const transform = `translate(-50%, -50%) scale(${scale}) rotate(${angle}deg)`;
+    return this.sanitizer.bypassSecurityTrustStyle(transform);
+  }
+
+
+  blur(img: HTMLImageElement, canvas: HTMLCanvasElement, radius: number) {
     const w = 512;
     const h = 512;
 
@@ -79,10 +108,16 @@ export class ParallaxComponent implements OnInit, CarouselChild {
     canvas.height = h;
 
     const context = canvas.getContext('2d');
-    context.clearRect( 0, 0, w, h );
     context.drawImage( img, 0, 0, w, h);
+    StackBlur.canvasRGBA( canvas, 0, 0, w, h, radius);
+    const imageData = context.getImageData(0, 0, w, h);
 
-    StackBlur.canvasRGB( canvas, 0, 0, w, h, 5);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      const k = Math.max(imageData.data[i], imageData.data[i + 1], imageData.data[i + 2]);
+      imageData.data[i + 3] = k;
+    }
+
+    context.putImageData(imageData, 0, 0);
   }
 
   restartRotate1() {
