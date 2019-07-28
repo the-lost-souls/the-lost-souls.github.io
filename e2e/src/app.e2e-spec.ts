6e02e2d5271fb9183e683173ddf72a9d16a425f2
CircleCI <torbjorv@gmail.com>
2019-07-28 15:16:55 -0700

    better name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to tls-pages!');
+    expect(page.getTitleText()).toEqual('Welcome to tls-home!');
   });
 
   afterEach(async () => {
---
+++ scripts/deploy-to-branch.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+TOKEN=$1
+BRANCH=$2
+BRANCH_DIR=branch-$BRANCH
+
+# All git output below is sent to dev/null to avoid exposing anything sensitive in build logs
+
+echo checkout $BRANCH
+mkdir $BRANCH_DIR
+cd $BRANCH_DIR
+
+git config --global user.name "CircleCI"  > /dev/null 2>&1
+git init  > /dev/null 2>&1
+git remote add --fetch origin https://$TOKEN@github.com/the-lost-souls/pages.git > /dev/null 2>&1
+
+git checkout $BRANCH > /dev/null 2>&1
+
+rm -rf * > /dev/null 2>&1
+# Revert the deletion of this one, we wanna keep it
+git checkout -- README.md
+
+# Copy angular app in here
+echo copy app
+cp -a "../dist/." .
+cp index.html 404.html
+
+echo add files
+git add -A > /dev/null 2>&1
+
+echo commit and push
+# need 'ci skip' to ignore this branch in CircleCI
+git commit --allow-empty -m "Deploy to branch '$BRANCH' [ci skip]"  > /dev/null 2>&1
+git push --force --quiet origin $BRANCH > /dev/null 2>&1
+
+echo cleanup
+cd ..
+rm -rf $BRANCH_DIR
